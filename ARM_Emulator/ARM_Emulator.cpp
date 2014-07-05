e490210df87cc1055c7d2ce06e264223f62fd8f0
Araj <nekron@gmail.com>
2014-07-05 21:04:34 +0200

    Added debug function

--- ARM_Emulator/ARM_Emulator.cpp
+++ ARM_Emulator/ARM_Emulator.cpp
@@ -41,6 +41,67 @@ enum OpCodes
 };
 
 /**
+* @brief prints the opcode type
+* @param [in] opcode  The opcode
+*/
+void debug_print(uint8_t opcode)
+{
+	switch (opcode)
+	{
+	case AND:
+		printf("and");
+		break;
+	case EOR:
+		printf("eor");
+		break;
+	case SUB:
+		printf("sub");
+		break;
+	case RSB:
+		printf("rsb");
+		break;
+	case ADD:
+		printf("add");
+		break;
+	case ADC:
+		printf("adc");
+		break;
+	case SBC:
+		printf("sbc");
+		break;
+	case RSC:
+		printf("rsc");
+		break;
+	case TST:
+		printf("tst");
+		break;
+	case TEQ:
+		printf("teq");
+		break;
+	case CMP:
+		printf("cmp");
+		break;
+	case CMN:
+		printf("cmn");
+		break;
+	case ORR:
+		printf("orr");
+		break;
+	case MOV:
+		printf("mov");
+		break;
+	case BIC:
+		printf("bic");
+		break;
+	case MVN:
+		printf("mvn");
+		break;
+	default:
+		break;
+	}
+}
+
+/**
 * @brief emulates ARM-v4 programs containing only MOV instructions in User32 mode
 * @param [in,out] registers  Array containing r0 to r15 followed by cpsr
 * @param [in] memory  Array containing 32-bit addressable program memory
@@ -49,6 +110,7 @@ enum OpCodes
 int ARM_User32_MOV_Emulator(unsigned int* registers, unsigned int* memory)
 {
 	Instruction inst = *(Instruction*)(memory[registers[Registers::PC]]);
+	debug_print(inst.generic_instruction.opcode);
 	if (inst.generic_instruction.opcode == OpCodes::MOV)
 	{
 
